544ad0cc107844ef950894a69f26ac6184a2348e
Eugene Yatsik <eugene.yatsik@yandex.ru>
2022-11-27 16:09:30 +0300

    Added fragments

---
+++ app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
@@ -0,0 +1,42 @@
+package ru.netology.nmedia.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
+import androidx.navigation.findNavController
+import ru.netology.nmedia.R
+import ru.netology.nmedia.activity.NewPostFragment.Companion.ARG_CONTENT
+
+class AppActivity : AppCompatActivity(R.layout.activity_app) {
+//    val binding by lazy { ActivityIntentHandlerBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(binding.root)
+
+        intent?.let {
+            if (it.action != Intent.ACTION_SEND) {
+                return@let
+            }
+
+            val text = it.getStringExtra(Intent.EXTRA_TEXT)
+            if (text.isNullOrBlank()) {
+//                Snackbar.make(binding.root, R.string.error_empty_content, LENGTH_INDEFINITE)
+//                    .setAction(android.R.string.ok) {
+//                        finish()
+//                    }.show()
+                return@let
+            }
+            intent.removeExtra(Intent.EXTRA_TEXT)
+            findNavController(R.id.nav_host_fragment_container).navigate(
+                R.id.feedFragmentToNewPostFragment.apply {
+                    bundleOf(ARG_CONTENT to text)
+//                    Bundle().apply {
+//                        textArg = text
+//                    }
+                }
+            )
+        }
+    }
+}

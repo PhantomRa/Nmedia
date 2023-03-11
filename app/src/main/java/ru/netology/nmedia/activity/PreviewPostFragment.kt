07158143d1c656a62376292ccfa2b590f607101d
Eugene Yatsik <eugene.yatsik@yandex.ru>
2023-03-11 17:54:38 +0300

    Fixed fragments

--- app/src/main/java/ru/netology/nmedia/activity/PreviewPostFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/PreviewPostFragment.kt
@@ -1,12 +1,18 @@
 package ru.netology.nmedia.activity
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import ru.netology.nmedia.R
+import ru.netology.nmedia.activity.NewPostFragment.Companion.textArg
+import ru.netology.nmedia.adapter.OnInteractionListener
+import ru.netology.nmedia.adapter.PostViewHolder
 import ru.netology.nmedia.databinding.FragmentPreviewPostBinding
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.presentation.PostViewModel
@@ -24,13 +30,15 @@ class PreviewPostFragment : Fragment(R.layout.fragment_preview_post) {
     private val viewModel: PostViewModel by viewModels(
         ownerProducer = ::requireParentFragment
     )
-    private val data = viewModel.data
-    private var previewPostId: Long? = 0L
+    private var previewPostId: Long = 0L
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            previewPostId = it.getString(arguments?.textArgPreview)?.toLongOrNull()
+        arguments?.textArgPreview.let {
+            if (it != null) {
+                previewPostId = it.toLong()
+            }
         }
     }
 
@@ -45,10 +53,48 @@ class PreviewPostFragment : Fragment(R.layout.fragment_preview_post) {
             false
         )
 
-        var post: Post? = null
+        val viewHolder = PostViewHolder(_binding!!.previewPost, object : OnInteractionListener {
+            override fun onLike(post: Post) {
+                viewModel.likeById(post.id)
+            }
 
-        data.observe(viewLifecycleOwner) { posts ->
-            post = posts.find { it.id == previewPostId }
+            override fun onShare(post: Post) {
+                viewModel.shareById(post.id)
+                val intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, post.content)
+                    type = "text/plain"
+                }
+
+                val shareIntent =
+                    Intent.createChooser(intent, getString(R.string.chooser_share_post))
+                startActivity(shareIntent)
+            }
+
+            override fun onEdit(post: Post) {
+                viewModel.edit(post)
+                findNavController().navigate(R.id.feedFragmentToNewPostFragment,
+                    Bundle().apply {
+                        textArg = post.content
+                    }
+                )
+            }
+
+            override fun onRemove(post: Post) {
+                viewModel.removeById(post.id)
+                if (findNavController().currentDestination?.id == R.id.previewPostFragment) {
+                    findNavController().navigateUp()
+                }
+            }
+
+            override fun onPlay(uri: String) {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+                startActivity(intent)
+            }
+        })
+
+        viewModel.data.observe(viewLifecycleOwner) {
+            viewModel.data.value?.find { it.id == previewPostId }?.let { viewHolder.bind(it) }
         }
 
         return binding.root
